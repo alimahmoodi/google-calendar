9efa3bf9f4e60e670d7b3c03d40fab9b0aaebbe4
mahmoodi <alimahmudi.alami@gmail.com>
2021-11-24 00:28:40 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { getMonth } from "./util";
+import CalendarHeader from "./CalendarHeader";
+import Month from "./Month";
+import Sidebar from "./Sidebar";
 
 function App() {
+  // console.table(getMonth());
+  // getMonth(11);
+  const [currentMonth, setCurrentMonth] = useState(getMonth());
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="h-screen flex flex-columns">
+        <CalendarHeader />
+        <div className="flex flex-1">
+          <Sidebar />
+          <Month month={currentMonth} />
+        </div>
+      </div>
     </div>
   );
 }
